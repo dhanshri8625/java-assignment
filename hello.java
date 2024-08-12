3fe8e1a53676d870c574f96945fb407096bc8b4d
dhanshri8625 <dhanshrighadge2004@gmail.com>
2024-08-12 21:56:19 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,6 @@
+import java.util.*;
+public class hello{
+    public static void main(String[]args){
+        System.out.println("hello all students" +"\n"+ "i am here to teach you how to print hello world in java");
+    }
+}
