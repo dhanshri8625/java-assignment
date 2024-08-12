520c87834de31eecab8217daa46ddf04c7ae0282
dhanshri8625 <dhanshrighadge2004@gmail.com>
2024-08-12 21:57:26 +0530

    Add files via upload

---
+++ even_or_odd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class even_or_odd {
+    public static void main(String[]args){
+        int n = 0;
+        Scanner S = new Scanner(System.in);
+        System.out.print("\n enter the number:");
+        n = S. nextInt();
+        if(n % 2 == 0){
+            System.out.println("entered number is even");
+        }
+        else{
+            System.out.println("entered number is Odd");
+        }
+
+
+    }
+}
